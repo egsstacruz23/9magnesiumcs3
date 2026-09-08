652760cdcbc8fdaf923a56ef91cfd799243a2c6f
egsstacruz23 <egsstacruz@brc.pshs.edu.ph>
2026-09-08 20:46:33 +0800

    Update classImplementation.py

--- q1/classImplementation.py
+++ q1/classImplementation.py
@@ -17,3 +17,25 @@ class Car:
     
 Car1 = (Toyata, 500000, Red)
 Car2 = (Tesla, 10000000, Silver)
+Car3 = (Ford, 750000, Yellow)
+Car4 = (BMW, 1000000, Black)
+
+Car1.accelerate()
+Car1.accelerate()
+Car1.accelerate()
+
+Car2.accelerate()
+Car2.accelerate()
+
+Car3.accelerate()
+Car3.accelerate()
+Car3.accelerate()
+Car3.accelerate()
+Car3.accelerate()
+
+Car4.accelerate()
+
+print(Car1.brand, Car1.price, Car1.color, Car1.get_speed())
+print(Car2.brand, Car2.price, Car2.color, Car2.get_speed())
+print(Car3.brand, Car3.price, Car3.color, Car3.get_speed())
+print(Car4.brand, Car4.price, Car4.color, Car4.get_speed())
