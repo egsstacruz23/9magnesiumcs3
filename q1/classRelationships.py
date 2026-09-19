d0f46cb0695ca46607c3678579b780e87c575f5f
egsstacruz23 <egsstacruz@brc.pshs.edu.ph>
2026-09-19 17:32:23 +0400

    Refactor Driver and Car classes for clarity

--- q1/classRelationships.py
+++ q1/classRelationships.py
@@ -1,9 +1,12 @@
+import random
+
 class Car:
     def __init__(self, brand, price, color):
         self.brand = brand
         self.price = price
         self.color = color
         self.__speed = 0
+        self.driver = None
 
     def accelerate(self):
         self.__speed += 10
@@ -14,6 +17,7 @@ class Car:
 
     def get_speed(self):
         return self.__speed
+
 
 class Driver:
     def __init__(self, race, condition, clothing):
@@ -27,21 +31,21 @@ class Driver:
         self.__luck += 3
 
     def check_crash(self):
-        check = random.randint(1,20)
+        chance = random.randint(1, 20)
 
-    if chance <= self.__luck:
-        return True
-    else:
-        return False
-  
-    def get_luck(self, car):
+        if chance <= self.__luck:
+            return True
+        else:
+            return False
+
+    def get_luck(self):
         return self.__luck
 
-    def add_cars(self):
+    def add_car(self, car):
         self.cars.append(car)
         car.driver = self
 
-    def display_info(self)
+    def display_info(self):
         print("Race:", self.race)
         print("Condition:", self.condition)
         print("Clothing:", self.clothing)
@@ -51,13 +55,15 @@ class Driver:
         for car in self.cars:
             print(car.brand, car.color)
 
+
 driver1 = Driver("Human", "Healthy", "Racing Suit")
 
-Car1 = Car("Toyota", 1200000, "Red")
-Car2 = Car("Honda", 1500000, "Blue")
-Car3 = Car("Ford", 2500000, "Black")
-Car4 = Car("BMW", 4000000, "White")
-Car5 = Car("Tesla", 3000000, "Silver")
+car1 = Car("Toyota", 1200000, "Red")
+car2 = Car("Honda", 1500000, "Blue")
+car3 = Car("Ford", 2500000, "Black")
+car4 = Car("BMW", 4000000, "White")
+car5 = Car("Tesla", 3000000, "Silver")
+
 
 driver1.add_car(car1)
 driver1.add_car(car2)
@@ -69,20 +75,23 @@ print("Cars connected")
 
 driver1.display_info()
 
+
 for car in driver1.cars:
     print(
         car.brand,
         "Price:", car.price,
-        "Color", car.color,
+        "Color:", car.color,
         "Speed:", car.get_speed(),
         "Driver Race:", car.driver.race,
     )
 
 driver1.luck()
 
-print("Updated Crash Chance:", driver1.get_luck(), "%")
+print("Updated Crash Chance:", driver1.get_luck(), "/20 rating")
 
 if driver1.check_crash():
     print("The driver crashed!")
 else:
     print("The driver did not crash.")
+
+# Needed research for random int (https://www.geeksforgeeks.org/python/python-random-module/)
