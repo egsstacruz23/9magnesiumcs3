d706a4da45b8b692e7d6337b6fe7b103e159e69e
egsstacruz23 <egsstacruz@brc.pshs.edu.ph>
2026-09-24 18:11:50 +0400

    Update advancedRelationships.py

--- q1/advancedRelationships.py
+++ q1/advancedRelationships.py
@@ -1,12 +1,19 @@
 import random
 
+
+class Engine:
+    def __init__(self, horsepower):
+        self.horsepower = horsepower
+
+
 class Car:
-    def __init__(self, brand, price, color):
+    def __init__(self, brand, price, color, horsepower):
         self.brand = brand
         self.price = price
         self.color = color
         self.__speed = 0
         self.driver = None
+        self.engine = Engine(horsepower)
 
     def accelerate(self):
         self.__speed += 10
@@ -17,6 +24,15 @@ class Car:
 
     def get_speed(self):
         return self.__speed
+
+
+class SportsCar(Car):
+    def __init__(self, brand, price, color, horsepower):
+        super().__init__(brand, price, color, horsepower)
+        self.turbo_power = 10
+
+    def turbo(self):
+        self.__speed = self.get_speed() + self.turbo_power
 
 
 class Driver:
@@ -58,11 +74,12 @@ class Driver:
 
 driver1 = Driver("Human", "Healthy", "Racing Suit")
 
-car1 = Car("Toyota", 1200000, "Red")
-car2 = Car("Honda", 1500000, "Blue")
-car3 = Car("Ford", 2500000, "Black")
-car4 = Car("BMW", 4000000, "White")
-car5 = Car("Tesla", 3000000, "Silver")
+car1 = Car("Toyota", 1200000, "Red", 150)
+car2 = Car("Honda", 1500000, "Blue", 180)
+car3 = Car("Ford", 2500000, "Black", 200)
+car4 = Car("BMW", 4000000, "White", 250)
+
+car5 = SportsCar("Tesla", 3000000, "Silver", 500)
 
 
 driver1.add_car(car1)
@@ -75,15 +92,24 @@ print("Cars connected")
 
 driver1.display_info()
 
-
 for car in driver1.cars:
     print(
         car.brand,
         "Price:", car.price,
         "Color:", car.color,
         "Speed:", car.get_speed(),
-        "Driver Race:", car.driver.race,
+        "Driver Race:", car.driver.race
     )
+
+print("Engine Horsepower:", car5.engine.horsepower)
+
+car5.accelerate()
+
+print("SportsCar Speed:", car5.get_speed())
+
+car5.turbo()
+
+print("SportsCar Speed after Turbo:", car5.get_speed())
 
 driver1.luck()
 
@@ -93,3 +119,6 @@ if driver1.check_crash():
     print("The driver crashed!")
 else:
     print("The driver did not crash.")
+
+# Debugged using programiz compiler
+# Used https://stackoverflow.com/questions/19861785/composition-and-aggregation-in-python to learn the difference of composition and aggregation
